a5ab232c39baffd1e374f4d5d9208fe06f8f7812
Yves Martinez <yves.oliver.m@gmail.com>
2024-06-12 16:25:37 -0400

    added models and schemas for cohort and student and required in app.js

---
+++ server/models/Student.model.js
@@ -0,0 +1,40 @@
+// ./models/cohort.model.js
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// CREATE SCHEMA
+// Schema - describes and enforces the structure
+const studentSchema = new Schema({
+  firstName: { type: String, required: true },
+  lastName: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true },
+  linkedinUrl: { type: String, default: "" },
+  languages: {
+    type: [String],
+    enum: [
+      "English",
+      "Spanish",
+      "French",
+      "German",
+      "Portuguese",
+      "Dutch",
+      "Other",
+    ],
+  },
+  program: {
+    type: String,
+    enum: ["Web Dev", "UX/UI", "Data Analytics", "Cybersecurity"],
+  },
+  background: { type: String, default: "" },
+  image: { type: String, default: "https://i.imgur.com/r8bo8u7.png" },
+  cohort: { type: Schema.Types.ObjectId, ref: "Cohort" },
+  projects: { type: [] },
+});
+
+// CREATE MODEL
+
+const Student = mongoose.model("Student", studentSchema);
+
+// EXPORT THE MODEL
+module.exports = Student;
